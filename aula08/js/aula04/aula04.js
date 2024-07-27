aa277667329f2fd9c6301518f14872545354d225
Was01 <80862278+Was01@users.noreply.github.com>
2024-07-27 20:41:30 -0300

    Add files via upload

---
+++ aula08/js/aula04/aula04.js
@@ -0,0 +1,19 @@
+//Criando um array
+let produtos=['Arroz','Feijão','Leite']
+
+//Adicionando um produto no final da lista
+produtos.push('Açucar')
+
+
+//Remover elemento no final da lista
+produtos.pop()
+produtos.pop()
+
+produtos.push('trigo')
+
+//Adicionando elementos no início da lista
+produtos.unshift('Uva','Maça')
+
+
+//Remover do início
+produtos.shift()
