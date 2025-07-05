eb6ccfedf1df8e3e5768ba8bf1a9452468442075
Was01 <80862278+Was01@users.noreply.github.com>
2025-07-05 20:49:47 -0300

    Update media_movel(potenciometro).ino

--- modulo2/media_movel(potenciometro).ino
+++ modulo2/media_movel(potenciometro).ino
@@ -1,13 +1,13 @@
-int media_movel(int pino){
+int media_movel(int pino, float *amostra){
   for(int i=n_pontos-1;i>0;i--){
-    amostras[i]=amostras[i-1];
+    amostra[i]=amostra[i-1];
    }
-   amostras[0]=analogRead(pino);
+   amostra[0]=analogRead(pino);
    
    float soma=0.0;
 
    for(int i=0;i<n_pontos-1;i++){
-     soma+=amostras[i];
+     soma+=amostra[i];
    }
    return soma/n_pontos;
 }
