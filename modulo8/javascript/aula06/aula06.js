81f44b8f750b60528cac7d2413d424cc3ba7b9bb
Was01 <80862278+Was01@users.noreply.github.com>
2026-08-04 19:45:57 -0300

    Update aula06.js

--- modulo8/javascript/aula06/aula06.js
+++ modulo8/javascript/aula06/aula06.js
@@ -1,4 +1,14 @@
+// Operadores de comparação
 
+// > maior que
+// < menor que
+// >= maior ou igual a 
+// <= menor ou igual a 
+// == igual a
+// === estritamente igual a
+// != diferente de
+// === estritamente igual a
+// !== estritamente diferente de 
 
 console.log('30'==30) //mesmo valor retorna true
 console.log('30'===30)// tipos diferentes retorna false 
