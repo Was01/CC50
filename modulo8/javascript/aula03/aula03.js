032e96ff3cefe52052d2d400f7fe79803fe74f79
Was01 <80862278+Was01@users.noreply.github.com>
2026-08-04 12:48:23 -0300

    Add files via upload

---
+++ modulo8/javascript/aula03/aula03.js
@@ -0,0 +1,20 @@
+//comentários
+
+/*
+
+    Texo
+
+*/
+
+//Cria a variável com o nome meuNome e atribui o valor Washington
+const meuNome='Washington'
+
+//Cria a variável com o nome result e atribui o valor da soma dos números de 1 a 10
+const result=1+2+3+4+5+6+7+8+9+10;
+
+
+//Imprime o valor da variável meuNome e result no console
+console.log(meuNome);
+
+//Imprime o valor da variável result no console
+console.log(result);
