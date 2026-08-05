8a68472fa02c7ec9b63cc007c01e68480b4e5196
Was01 <80862278+Was01@users.noreply.github.com>
2026-08-05 17:25:33 -0300

    Add files via upload

---
+++ modulo8/javascript/aula07/aula07.js
@@ -0,0 +1,11 @@
+// && E (and)
+// || ou (or)
+// ! negação (not)
+
+const cake=false
+const iceCream=true
+
+console.log(cake && iceCream) // false
+console.log(cake || iceCream) // true
+
+console.log(!cake) // true
